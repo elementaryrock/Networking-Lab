2d26d3b7cd50790a2042acc437cb154ee4047cbc
Maanas MS <109026282+elementaryrock@users.noreply.github.com>
2026-05-19 13:13:44 +0530

    Modified ftp server

--- ftps.c
+++ ftps.c
@@ -1,13 +1,11 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(){
-    char buf[100];
-    char fileContent[1024];
+    char buf[100],fileContent[1024];
     int sock_desc,client_desc;
     socklen_t len;
     struct sockaddr_in server,client;
@@ -16,7 +14,7 @@ int main(){
 
     server.sin_family=AF_INET;
     server.sin_addr.s_addr=INADDR_ANY;
-    server.sin_port=htons(8090);
+    server.sin_port=htons(8085);
 
     bind(sock_desc,(struct sockaddr*)&server,sizeof(server));
 
@@ -25,21 +23,25 @@ int main(){
     len=sizeof(client);
     client_desc=accept(sock_desc,(struct sockaddr*)&client,&len);
 
-    recv(client_desc,buf,sizeof(buf),0);
-    buf[strcspn(buf,"\n")] = '\0';
-    printf("%s",buf);
+    recv(client_desc,buf,100,0);
+    buf[strcspn(buf,"\n")]='\0';
+    printf("\nMessage received from client: %s\n",buf);
 
-    FILE *fp=fopen(buf,"r");
+    FILE *fp;
+    fp=fopen(buf,"r");
     if (fp==NULL){
+        printf("File not Found");
         strcpy(fileContent,"ERROR");
-        printf("File not found");
+        send(client_desc,fileContent,sizeof(fileContent),0);
     }
     else{
-        int readCount = fread(fileContent,1,sizeof(fileContent)+1,fp);
-        fileContent[readCount]='\0';
-    }
-    send(client_desc,fileContent,sizeof(fileContent)+1,0);
-    fclose(fp);
+        int readc=fread(fileContent,1,sizeof(fileContent),fp);
+        fileContent[readc]='\0';
+        send(client_desc,fileContent,sizeof(fileContent),0);
+        fclose(fp);
+        }
+
     close(client_desc);
     close(sock_desc);
-}
+    }
+
